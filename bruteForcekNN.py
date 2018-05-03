62e33060ce118e34bd455c5ffd4f302b6643277c
Alex Taylor <act3@williams.edu>
2018-05-03 11:29:08 -0400

    adds makeTable method/bruteForce file

---
+++ bruteForcekNN.py
@@ -0,0 +1,25 @@
+#does kNN on our data
+# (c) 2018 Alex C. Taylor
+
+import csv
+import math
+
+#merges the train and background tables into one
+def makeTable():
+    trainingSet = []
+    with open('train.csv') as csvfile:
+        reader = csv.reader(csvfile, delimiter= ',')
+        for r in reader:
+            trainingSet.append(r)
+    with open('background.csv') as csvfile:
+        reader = csv.reader(csvfile, delimiter= ',')
+        tIndex = 0
+        for r in reader:
+            if(tIndex < len(trainingSet) and r[0] == trainingSet[tIndex][0]):
+                trainingSet[tIndex].append(r)
+                tIndex += 1
+    return trainingSet
+
+#do the kNN 
+
+makeTable()
