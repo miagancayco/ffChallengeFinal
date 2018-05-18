8737cf83fc951ad79420a56a8826f02147121bb1
Alex Taylor <act3@williams.edu>
2018-05-17 23:02:20 -0400

    makes linReg use test data

--- linearReg.py
+++ linearReg.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 import csv
 
-def parseData():
+def parseDataOld():
     familyData = [] # list of child lists that will return
     justAttributes = []
     gpaData = []
@@ -26,27 +26,64 @@ def parseData():
                     family.update(row)
     return familyData
 
-def doReg(data):
-    df = pd.DataFrame(data=data).fillna(0)
-    print(df)
-    target = pd.DataFrame(data=data, columns=["gpa"]).fillna(0)
+def parseData():
+    trainSet = []
+    testSet = []
+    familyData = (trainSet,testSet) # list of child lists that will return
+    with open('test_linReg.csv') as csvfile:
+        tReader = csv.DictReader(csvfile)
+        for row in tReader:
+            testSet.append(row)
+    with open('train_no_NA.csv') as csvfile:
+        tReader = csv.DictReader(csvfile)
+        for row in tReader:
+            trainSet.append(row)
+    with open('backgroundcopylarger_no_NA.csv') as csvfile:
+        bReader = csv.DictReader(csvfile)
+        for row in bReader:
+            famID = float(row.get('\ufeffchallengeID'))
+            #separate test and training sets
+            for family in testSet:
+                testFamID = float(family.get('\ufeffchallengeID'))
+                if testFamID > famID:
+                    break
+                if testFamID == famID:
+                    family.update(row)
+            for family in trainSet:
+                trainFamID = float(family.get('challengeID'))
+                #add corresponding background information to appropriate family
+                #in familyData by checking ID number
+                if trainFamID > famID:
+                    break
+                if famID == trainFamID:
+                    family.update(row)
+                    del family['\ufeffchallengeID']
+    return familyData
+
+def doReg(trainData,testData):
+    X_train = pd.DataFrame(data=trainData).fillna(0)
+    print(X_train)
+    target = pd.DataFrame(data=trainData, columns=["gpa"]).fillna(0)
     print(target)
-    X = df
-    #print(X)
     y = target[["gpa"]]
     lm = linear_model.LinearRegression()
-    model = lm.fit(X,y)
-    predictions = lm.predict(X)
+    lm.fit(X_train,y)
+    #lm.score(X_train,y)
+    X_test = pd.DataFrame(data=testData).fillna(0)
+    print(X_test)
+    predictions = lm.predict(X_test)
     print("gpa predictions")
     print(predictions[0:30])
-    target = pd.DataFrame(data=data, columns=["grit"]).fillna(0)
+    target = pd.DataFrame(data=trainData, columns=["grit"]).fillna(0)
     y = target[["grit"]]
-    model = lm.fit(X,y)
-    predictions = lm.predict(X)
+    lm.fit(X_train,y)
+    predictions = lm.predict(X_test)
     print("grit predictions")
     print(predictions[0:30])
 
-data = parseData()
+tuple = parseData()
+trainData = tuple[0]
+testData = tuple[1]
 
-doReg(data)
+doReg(trainData,testData)
 
